d61493f18be9705893aab3f3a3e1402707d122da
Akshat <Akshat@LAPTOP-42T5OINN>
2020-05-31 09:56:43 +0530

    First commit

---
+++ Hello world/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int sum = add(20,10);
+		System.out.println(sum);
+	}
+public static int add (int i,int j)
+{
+	return i+j;
+	
+	}
+}
